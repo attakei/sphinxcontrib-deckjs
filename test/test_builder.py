e2372e45aaba4916d3d57d76ff043bf6116f7775
Kazuya Takei <attakei@gmail.com>
2016-09-27 03:07:52 +0900

    Test contents

--- test/test_builder.py
+++ test/test_builder.py
@@ -10,5 +10,7 @@ def test_builder():
     @with_app(buildername='deckjs', srcdir=src_dir, copy_srcdir_to_tmpdir=True)
     def _test_builder(app, status, warning):
         app.build()
+        html = (app.outdir / 'contents.html').read_text()
+        assert 'Slide title' in html
 
     _test_builder()
