8857273401d55b0f2787019c9b3056e0a4d9c013
Kazuya Takei <attakei@gmail.com>
2016-09-27 03:53:32 +0900

    Implement writing section by Translator

--- sphinxcontrib/deckjs.py
+++ sphinxcontrib/deckjs.py
@@ -5,6 +5,7 @@
     :copyright: attakei. All Rights Reserved.
 """
 from sphinx.builders.html import StandaloneHTMLBuilder
+from sphinx.writers.html import HTMLTranslator
 
 
 __version__ = '0.0.1'
@@ -14,6 +15,26 @@ class DeckjsBuilder(StandaloneHTMLBuilder):
     name = 'deckjs'
     copysource = False
     add_permalinks = False
+
+    def init_translator_class(self):
+        if self.translator_class is not None:
+            pass
+        else:
+            self.translator_class = DeckjsTranslator
+
+
+class DeckjsTranslator(HTMLTranslator):
+    def visit_section(self, node):
+        if self.section_level != 0:
+            self.body.append('</section>\n')
+        self.section_level += 1
+        self.body.append(
+            self.starttag(node, 'section', CLASS='slide'))
+
+    def depart_section(self, node):
+        self.section_level -= 1
+        if self.section_level == 0:
+            self.body.append('</section>\n')
 
 
 def setup(app):
